c9943f15519523b8ca22345f4cf64e9a6a96a11c
Carmelo Scollo <carmelo.scollo@gmail.com>
2024-01-01 20:13:32 +0100

    Add a hello world example with test

---
+++ helloworld/hello.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+	italian = "Italian"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	italianHelloPrefix = "Ciao, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case italian:
+		prefix = italianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
