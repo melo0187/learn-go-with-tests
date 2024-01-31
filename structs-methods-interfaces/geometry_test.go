a3884da1e7fe85d4b4b684abb401caac8c04f31f
itd: Carmelo Scollo <carmelo.scollo@meisterplan.com>
2024-01-31 14:28:58 +0100

    Add a "table driven test"
    Basically a parameterized test.

--- structs-methods-interfaces/geometry_test.go
+++ structs-methods-interfaces/geometry_test.go
@@ -13,6 +13,14 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
@@ -22,14 +30,7 @@ func TestArea(t *testing.T) {
 		}
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
-
+	for _, tt := range areaTests {
+		checkArea(t, tt.shape, tt.want)
+	}
 }
