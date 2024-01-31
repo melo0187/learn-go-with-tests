1d31da3bca5e5ca7b0d64ec0e288b0e59874eaed
itd: Carmelo Scollo <carmelo.scollo@meisterplan.com>
2024-01-31 14:41:18 +0100

    Improve areaTests input readability
    We can name the fields of the structs to make the inputs easier to read
    and understand.

--- structs-methods-interfaces/geometry_test.go
+++ structs-methods-interfaces/geometry_test.go
@@ -1,6 +1,8 @@
 package structsmethodsinterfaces
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -17,9 +19,9 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	checkArea := func(t testing.TB, shape Shape, want float64) {
