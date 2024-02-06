176932362b273d8b39707a63e204a37058e78035
itd: Carmelo Scollo <carmelo.scollo@meisterplan.com>
2024-02-06 21:30:56 +0100

    Improve table driven test's error output
    Achieved by printing the entire Shape under test, adding a name to the
    testcase and using t.Run to use the name as test identifier.

--- structs-methods-interfaces/geometry_test.go
+++ structs-methods-interfaces/geometry_test.go
@@ -16,12 +16,13 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	checkArea := func(t testing.TB, shape Shape, want float64) {
@@ -29,11 +30,14 @@ func TestArea(t *testing.T) {
 		got := shape.Area()
 
 		if got != want {
-			t.Errorf("got %g want %g", got, want)
+			t.Errorf("%#v got %g want %g", shape, got, want)
 		}
 	}
 
 	for _, tt := range areaTests {
-		checkArea(t, tt.shape, tt.want)
+		t.Run(tt.name, func(t *testing.T) {
+			checkArea(t, tt.shape, tt.hasArea)
+		})
+
 	}
 }
