04a1576aafabadc21029c8de2c6b71312724b371
itd: Carmelo Scollo <carmelo.scollo@meisterplan.com>
2024-02-15 14:18:56 +0100

    Refactor Dictionary.Add to never update
    Learn the switch statement in the process.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,10 +1,15 @@
 package maps
 
-import "errors"
+import (
+	"errors"
+)
 
 type Dictionary map[string]string
 
-var ErrDefinitionNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrDefinitionNotFound = errors.New("could not find the word you were looking for")
+	ErrWordExists         = errors.New("provided word already has a definition")
+)
 
 func (d Dictionary) Search(word string) (definition string, err error) {
 	definition, ok := d[word]
@@ -15,6 +20,16 @@ func (d Dictionary) Search(word string) (definition string, err error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrDefinitionNotFound:
+		d[word] = definition
+		return nil
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
 }
